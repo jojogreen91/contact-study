9234317bbbf937fb07a1c8a529e5f87b281b37b2
jojogreen91 <iawbg13@gmail.com>
2021-07-30 16:47:26 +0900

    Chapter 03. Jpa - 02.03.04. Lombok

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,50 +1,40 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @NonNull
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDateTime birthday;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 }
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,14 +25,32 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("jo");
         person.setAge(20);
+        person.setBloodType("O");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
 
         List<Person> people = personRepository.findAll();
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("jo");
         assertThat(people.get(0).getAge()).isEqualTo(20);
+        assertThat(people.get(0).getBloodType()).isEqualTo("O");
+    }
+
+    @Test
+    void equalsAndHashCodeTest () {
+        Person person1 = new Person("jo", 20, "O");
+        Person person2 = new Person("jo", 20, "O");
+
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+        System.out.println(person1.equals(person2));
+
+        Map<Person, Integer> testMap = new HashMap<>();
+        testMap.put(person1, person1.getAge());
+
+        System.out.println(testMap);
+        System.out.println(testMap.get(person2));
     }
 }
