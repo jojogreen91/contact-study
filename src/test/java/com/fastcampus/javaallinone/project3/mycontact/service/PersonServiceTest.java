3119665f4894f501c784a679b9c4687b863a70f3
jojogreen91 <iawbg13@gmail.com>
2021-08-05 13:42:11 +0900

    Chapter 08. Mock-Test- 03. Service Test #3

--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonServiceTest.java
@@ -2,10 +2,12 @@ package com.fastcampus.javaallinone.project3.mycontact.service;
 
 import com.fastcampus.javaallinone.project3.mycontact.controller.dto.PersonDto;
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
+import com.fastcampus.javaallinone.project3.mycontact.domain.dto.Birthday;
 import com.fastcampus.javaallinone.project3.mycontact.repository.PersonRepository;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -69,11 +72,66 @@ class PersonServiceTest {
     @Test
     void put () {
 
-        PersonDto personDto = PersonDto.of("jo", "coding", "부산", LocalDate.of(1991, 10, 14), "student", "010-5224-1660");
+        personService.put(mockPersonDto());
 
-        personService.put(personDto);
-
-        // Mockito 에서의 검증 방식
+        // Mockito 에서의 검증 방식, 리턴 값이 void 일때 사용
         verify(personRepository, times(1)).save(any(Person.class));
+    }
+
+    @Test
+    void modifyIfPersonNotFound () {
+
+        when (personRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        // Exception 이 발생한다는 것을 확인하는 코드
+        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+    }
+
+    @Test
+    void modifyPersonIfNameIsDifferent () {
+
+        when (personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("kim")));
+
+        // Exception 이 발생한다는 것을 확인하는 코드
+        assertThrows(RuntimeException.class, () -> personService.modify(1L, mockPersonDto()));
+    }
+
+    @Test
+    void modify () {
+
+        when (personRepository.findById(1L))
+                .thenReturn(Optional.of(new Person("jo")));
+
+        // Exception 이 발생한다는 것을 확인하는 코드
+        personService.modify(1L, mockPersonDto());
+
+        // Mockito 에서의 검증 방식, 리턴 값이 void 일때 사용
+//        verify(personRepository, times(1)).save(any(Person.class));
+        verify(personRepository, times(1)).save(argThat(new IsPersonWillBeUpdated()));
+    }
+
+    private PersonDto mockPersonDto () {
+        return PersonDto.of("jo", "coding", "부산", LocalDate.of(1991, 10, 14), "student", "010-5224-1660");
+    }
+
+
+    // modify 할 때 세이브된 Entity 가 제대로 update 가 되었는지 확인하기 위한 클래스, Person 의 set 메서드가 제대로 작동하는지 확인하는 클래스
+    private static class IsPersonWillBeUpdated implements ArgumentMatcher<Person> {
+
+        @Override
+        public boolean matches(Person person) {
+            return equals(person.getName(), "jo")
+                    && equals(person.getHobby(), "coding")
+                    && equals(person.getAddress(), "부산")
+                    && equals(person.getBirthday(), Birthday.of(LocalDate.of(1991, 10, 14)))
+                    && equals(person.getJob(), "student")
+                    && equals(person.getPhoneNumber(), "010-5224-1660");
+        }
+
+        private boolean equals(Object actual, Object expected) {
+            return expected.equals(actual);
+        }
     }
 }
